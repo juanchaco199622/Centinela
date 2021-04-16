b8e3400889f5ea2a4b7991479b1806c08d6c8343
Luis Armando Saiz Ilvira <LUISAIIL@Finesa.local>
2021-04-16 18:18:30 -0500

    Se deja funcional el editar perfil

--- src/Layout/Auth/Profile/EditProfile.js
+++ src/Layout/Auth/Profile/EditProfile.js
@@ -1,102 +1,181 @@
 import firestore from '@react-native-firebase/firestore';
-import React, { useState } from 'react'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React, { useEffect, useState } from 'react'
 import { Title, Button, Avatar, Divider, TextInput } from 'react-native-paper'
 import {
   StyleSheet,
   Text,
   View,
+  ScrollView,
+  Alert
 } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import auth from '@react-native-firebase/auth'
+import auth from '@react-native-firebase/auth';
+import RNPickerSelect from 'react-native-picker-select';
 
-export default function EditProfile({navigation}) {
-  const user = auth().currentUser // Variable del servicio usuario
+//Inicio de funcion
+export default function EditProfile({route, navigation}) {
 
+//Declaracion de variables
+  const user = auth().currentUser
+  const data = route.params.state;
   const [state, setState] = useState({
-      nombres : "",
-      apellidos : "",
-      correo: "",
-      rol :"",
-      grupo :""
-  },{setNombres: ""})
-  
+    nombres: data.nombres, 
+    apellidos: data.apellidos,
+    correo: data.correo,
+    rol: data.rol,
+    grupo: data.grupo
+  })
+  const [ramas, setramas] = useState('')
+  const rama = [
+    { label: "Cachorros", value: "Cachorro" },
+    { label: "Lobatos", value: "Lobato" },
+    { label: "Webelos", value: "Webelo" },
+    { label: "Scouts", value: "Scout" },
+    { label: "Rovers", value: "Rover" },
+  ];
+  const rol = [
+    { label: "Administrador", value: "Administrador" },
+    { label: "Acudiente", value: "Acudiente" },
+  ];
+//Obtener datos de firestore
+useEffect(()=>{
+  let isSubscribed = true
   firestore()
-    .collection('Usuario')
-    .doc(user.uid)
+    .collection('Grupo')
+    .doc('7YmkzBGtRNw5IJK15zex')
     .onSnapshot((doc) => {
-      if(doc.exists){
-        setState({
-          nombres:doc.data().nombres, 
-          apellidos:doc.data().apellidos,
-          correo:doc.data().correo,
-          rol:doc.data().id_rol,
-          grupo:doc.data().id_grupo
-        });
+      if(isSubscribed){
+        const datos = doc.json();
+        console.log(datos);
       }
     });
-
-    const styles = StyleSheet.create({
-      roundButton: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10,
-        backgroundColor: '#6200ee',
-      },
-      profileHeader: {flexDirection:"row", padding:10, backgroundColor:"#fff"}
-    });
-
-    return (
-      <SafeAreaView>
-        <View style={styles.profileHeader}>
-          <Avatar.Image
-            source={{
-              uri: 'https://reactnativeelements.com//img/avatar/avatar--edit.jpg',
-            }}
-            size={80}
-          />
-          <View style={{marginLeft:10}}>
-            <Title style={{marginTop:25}}>Editar Perfil</Title>
-          </View>
-        </View>
-        <Divider/>
-        <View style={{padding:10}}>
-              <TextInput
-                mode='outlined'
-                label='Nombres'
-                autoCapitalize='none'
-                value={state.nombres}
-              />
-        </View>
-        <View style={{padding:10}}>
-              <TextInput
-                mode='outlined'
-                label='Apellidos'
-                autoCapitalize='none'
-                value={state.apellidos}
-              />
-        </View>
-        <View style={{padding:10}}>
-              <TextInput
-                mode='outlined'
-                label='Rol'
-                autoCapitalize='none'
-                value={state.rol}
-              />
-        </View>
-        <View style={{padding:10}}>
-              <TextInput
-                mode='outlined'
-                label='Rama'
-                autoCapitalize='none'
-                value={state.grupo}
-              />
-        </View>
-        <Divider/>
-        <View style={{alignItems:"flex-end", padding:10}}>
-        <Button icon="floppy" color = "#fff" uppercase={false} style={styles.roundButton} onPress={()=>navigation.navigate('Perfil')}>Guardar</Button>
-        </View>
-        
-      </SafeAreaView>
-    )    
+  return () => isSubscribed = false
+},[]) 
+//Estilos
+  const styles = StyleSheet.create({
+    roundButton: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
+      backgroundColor: '#6200ee',
+    },
+    profileHeader: {flexDirection:"row", padding:10, backgroundColor:"#fff"}
+  });
+  const pickerSelectStyles = StyleSheet.create({
+    inputAndroid: {
+      fontSize: 16,
+      paddingHorizontal: 15,
+      paddingVertical: 13,
+      borderWidth: 1,
+      borderColor: 'gray',
+      borderRadius: 5,
+      color: 'black',
+      paddingRight: 30,
+    },
+  });
+//Funciones de acciones
+  const updateProfile = async () =>{
+    let error = true
+    if(state.nombres === '' || state.apellidos === ''){
+      Alert.alert(
+        null,
+        'Completa todos los campos',
+        [
+          {
+            text: 'OK'
+          },
+        ],
+        {cancelable: false},
+      );
+    } else {
+        await firestore().collection('Usuario').doc(user.uid).update(
+          {
+            nombres: state.nombres,
+            apellidos: state.apellidos,
+            rol: state.rol,
+            grupo: state.grupo,
+          }
+        ).then(() => {
+          error = false
+        });
+      }
+    if(!error){
+      Alert.alert(
+        null,
+        'Perfil actualizado correctamente',
+        [
+          {
+            text: 'OK', 
+            onPress: () => navigation.navigate('Perfil')
+          },
+        ],
+        {cancelable: false},
+      );
+    }
   }
+  const handleChangeText = (name, value )=>{
+      setState({...state,[name]:value})
+  }
+//Render
+  return (
+    <SafeAreaView>
+      <ScrollView>
+      <View style={styles.profileHeader}>
+        <Avatar.Image
+          source={{
+            uri: 'https://reactnativeelements.com//img/avatar/avatar--edit.jpg',
+          }}
+          size={80}
+        />
+        <View style={{marginLeft:10}}>
+          <Title style={{marginTop:25}}>Editar Perfil</Title>
+        </View>
+      </View>
+      <Divider/>
+      <View style={{padding:10}}>
+        <Text>Nombres</Text>
+        <TextInput
+          mode='outlined'
+          returnKeyType={"next"} placeholder="Nombres"
+          onChangeText={(text) => handleChangeText('nombres', text)}
+          value={state.nombres}
+        />
+      </View>
+      <View style={{padding:10}}>
+        <Text>Apellidos</Text>
+        <TextInput
+          mode='outlined'
+          returnKeyType={"next"} placeholder="Apellidos"
+          onChangeText={(text) => handleChangeText('apellidos', text)}
+          value={state.apellidos}
+        />
+      </View> 
+      <View style={{padding:10}}>
+        <Text>Rol</Text>
+        <RNPickerSelect style={pickerSelectStyles}
+          placeholder= {{}}
+          onValueChange={(value) => handleChangeText('rol', value)}
+          useNativeAndroidPickerStyle={false}
+          value={state.rol}
+          items={rol}
+        />
+      </View>
+      <View style={{padding:10}}>
+        <Text>Rama</Text>
+        <RNPickerSelect style={pickerSelectStyles}
+          placeholder= {{}}
+          onValueChange={(value) => handleChangeText('grupo', value)}
+          useNativeAndroidPickerStyle={false}
+          value={state.grupo}
+          items={rama}
+        />
+      </View>
+      <Divider/>
+      <View style={{alignItems:"flex-end", padding:10}}>
+      <Button icon="floppy" color = "#fff" uppercase={false} style={styles.roundButton} 
+      onPress={()=>updateProfile()}>Guardar</Button>
+      </View>
+      </ScrollView> 
+    </SafeAreaView>
+  )    
+}
--- src/Layout/Auth/Profile/Profile.js
+++ src/Layout/Auth/Profile/Profile.js
@@ -1,5 +1,5 @@
 import firestore from '@react-native-firebase/firestore';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Title, Caption, Button, Avatar, Divider } from 'react-native-paper'
 import {
@@ -10,87 +10,82 @@ import {
 import { SafeAreaView } from 'react-native-safe-area-context';
 import auth from '@react-native-firebase/auth'
 
+//Inicio de funcion
 export default function Profile({navigation}) {
 
-  const user = auth().currentUser // Variable del servicio usuario
-
+//Declaracion de variables
+  const user = auth().currentUser;
   const [state, setState] = useState({
       nombres : "",
       apellidos : "",
       correo: "",
       id_rol :"",
       grupo :""
-  })
-  
-  firestore()
-    .collection('Usuario')
-    .doc(user.uid)
-    .onSnapshot((doc) => {
-      if(doc.exists){
-        setState({
-          nombres:doc.data().nombres, 
-          apellidos:doc.data().apellidos,
-          correo:doc.data().correo,
-          id_rol:doc.data().id_rol,
-          grupo:doc.data().id_grupo
-        });
-      }else{
-        //alert('no datos')
-        setState({
-          nombres:"", 
-          apellidos:"",
-          correo:"",
-          rol:"",
-          grupo:""
-        });
-      }
-    });
-
-    const styles = StyleSheet.create({
-      roundButton: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10,
-        backgroundColor: '#6200ee',
-      },
-      profileHeader: {flexDirection:"row", padding:10, backgroundColor:"#fff"}
-    });
-
-    return (
-      <SafeAreaView>  
-        <View style={styles.profileHeader}>
-            <Avatar.Image
-              source={{
-                uri: 'https://reactnativeelements.com//img/avatar/avatar--edit.jpg',
-              }}
-              size={80}
-            />
-            <View style={{marginLeft:10}}>
-              <Title style={{marginTop:10}}>{ state.nombres} {state.apellidos}</Title>
-              <Caption>{ state.grupo}</Caption>
-            </View>
+  });
+//Obtener datos de firestore
+  useEffect(()=>{
+    let isSubscribed = true
+    firestore()
+      .collection('Usuario')
+      .doc(user.uid)
+      .onSnapshot((doc) => {
+        if(isSubscribed){
+          setState({
+            nombres:doc.data().nombres, 
+            apellidos:doc.data().apellidos,
+            correo:doc.data().correo,
+            rol:doc.data().rol,
+            grupo:doc.data().grupo
+          });
+        }
+      });
+    return () => isSubscribed = false
+  },[]) 
+//Estilos
+  const styles = StyleSheet.create({
+    roundButton: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
+      backgroundColor: '#6200ee',
+    },
+    profileHeader: {flexDirection:"row", padding:10, backgroundColor:"#fff"}
+  });
+//Render
+  return (
+    <SafeAreaView>
+      <View style={styles.profileHeader}>
+          <Avatar.Image
+            source={{
+              uri: 'https://reactnativeelements.com//img/avatar/avatar--edit.jpg',
+            }}
+            size={80}
+          />
+          <View style={{marginLeft:10}}>
+            <Title style={{marginTop:10}}>{ state.nombres} {state.apellidos}</Title>
+            <Caption>{ state.grupo}</Caption>
+          </View>
+      </View>
+      <Divider/>
+        <View style={{flexDirection:"row", padding:10}}>
+          <Icon name="account" size={25}/>
+          <Text style={{marginLeft:10,marginTop:2}}>{ state.rol }</Text>
         </View>
-        <Divider/>
-          <View style={{flexDirection:"row", padding:10}}>
-            <Icon name="account" size={25}/>
-            <Text style={{marginLeft:10,marginTop:2}}>{ state.id_rol }</Text>
-          </View>
-          <View style={{flexDirection:"row", padding:10}}>
-            <Icon name="email" size={25}/>
-            <Text style={{marginLeft:10,marginTop:2}}>{ user.email }</Text>
-          </View>
-          <View style={{flexDirection:"row", padding:10}}>
-            <Icon name="account" size={25}/>
-            <Text style={{marginLeft:10,marginTop:2}}>{ user.uid }</Text>
-          </View>
-        <Divider/>
-        <View style={{alignItems:"flex-end", padding:10}}>
-          <Button icon="pencil" color = "#fff" uppercase={false} 
-            style={styles.roundButton} 
-            onPress={()=>navigation.navigate('EditProfile')}
-          >Editar perfil</Button>
+        <View style={{flexDirection:"row", padding:10}}>
+          <Icon name="email" size={25}/>
+          <Text style={{marginLeft:10,marginTop:2}}>{ user.email }</Text>
         </View>
-        
-      </SafeAreaView>
-    )    
-  }
+        {/* <View style={{flexDirection:"row", padding:10}}>
+          <Icon name="account" size={25}/>
+          <Text style={{marginLeft:10,marginTop:2}}>{ user.uid }</Text>
+        </View> */} 
+      <Divider/>
+      <View style={{alignItems:"flex-end", padding:10}}>
+        <Button icon="pencil" color = "#fff" uppercase={false} 
+          style={styles.roundButton} 
+          onPress={()=>navigation.navigate('EditProfile',{state})}
+        >Editar perfil</Button>
+      </View>
+    </SafeAreaView>
+  )    
+}
