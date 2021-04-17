c2933640cc8dc24bdeb254d7d054b28c73107b9e
unknown <juan199622@hotmail.com>
2021-04-17 01:19:36 -0500

    DISENO LIST USUARIOS

--- src/Layout/Auth/ListUsers.js
+++ src/Layout/Auth/ListUsers.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { ActivityIndicator, FlatList, View, Text } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
+import { ListItem } from 'react-native-elements';
 
 export default function ListUsers(){
 //const ListUsers = props => {
@@ -30,37 +31,17 @@ export default function ListUsers(){
         return () => subscriber();
       }, []);
 
-
-        /*firestore().collection('Usuario').onSnapshot(querySnapshot =>{
-
-            const users = [];
-
-            querySnapshot.docs.forEach((doc) =>{
-
-                const {nombres, apellidos, email,id_rol,id_grupo} = doc.data();
-                    users.push({
-                        id : doc.id,
-                        nombres,
-                        apellidos,
-                        email,
-                        id_rol,
-                        id_grupo,
-                    });
-                setUsers(users)
-                //console.log(doc.data())
-            });
-        })*/
-   /* }, [])*/
-
    return (
     <FlatList
       data={users}
       renderItem={({ item }) => (
-        <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>User ID: {item.id}</Text>
-          <Text>User Name: {item.nombres}</Text>
-          <Text>User Apellidos: {item.apellidos}</Text>
-        </View>
+        <ListItem key={item.id} bottomDivider>
+           
+            <ListItem.Content>
+            <ListItem.Title>{item.nombres+' ' + item.apellidos}</ListItem.Title>
+            <ListItem.Subtitle>{item.id_grupo}</ListItem.Subtitle>
+            </ListItem.Content>
+      </ListItem>
       )}
     />
   );
