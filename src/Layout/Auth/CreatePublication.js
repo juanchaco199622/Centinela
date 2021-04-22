48a3c6a2a525441a8a54b9480789d9214dbc9e87
unknown <cristhian.andrade@uao.edu.co>
2021-04-21 20:42:03 -0500

    CreatePublication.js

--- src/Layout/Auth/CreatePublication.js
+++ src/Layout/Auth/CreatePublication.js
@@ -1,7 +1,7 @@
-import React, { useState, useRef , Text} from 'react'
-import { View, StyleSheet, TextInput, TouchableOpacity, Image} from 'react-native'
+import React, { useState, useRef } from 'react'
+import { View, StyleSheet, TextInput, TouchableOpacity, Image, Text} from 'react-native'
 import {IconButton, Subheading, ProgressBar} from 'react-native-paper'
-import { Icon, Button } from 'react-native-elements';
+import { Icon, Button, Header } from 'react-native-elements';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 import { Picker } from '@react-native-picker/picker';
@@ -9,6 +9,7 @@ import ImagePicker from 'react-native-image-picker'
 import {imagePickerOptions} from '../../Utils';
 import RBSheet from "react-native-raw-bottom-sheet";
 import {useUploadImagePreRegister} from '../../Hooks'
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
  
 
 export default function CreatePublication() {
@@ -90,114 +91,202 @@ export default function CreatePublication() {
     }
 
     const styles = StyleSheet.create({
+        Pheader:{
+            
+            backgroundColor:'#b10909'
+
+
+        },
+        Pbody:{
+            width:'85%',
+            height:750,
+            alignContent:'center',
+            //justifyContent:'center',
+            alignSelf:'center',
+            backgroundColor:'#e8e8e8',
+            
+
+        },
+     
+
         container: {
-            backgroundColor: '#F5FCFF', /**'#F5FCFF' */
+            backgroundColor: 'white', /**'#F5FCFF' */
+            
+
+        },
+
+        inputGroup: {
+            width:'90%',
+            backgroundColor:'white',
+            marginTop:10,
+            alignSelf:'center',
+            textAlignVertical:'top',
+            
+
+            //justifyContent:'flex-end',
+            
+        },
+        TextGroup:{
+            width:'90%',
+            marginTop:10,
+            alignSelf:'center',
         }
+
+
     })
 
     return (
-
-        <View style={styles.container}>
-            <View style={styles.inputGroup}>
-                <TextInput
-                    placeholder='Titulo:'
-                    onChangeText={(value) => handleChangeText("titulo", value)}
-                />
-            </View>
-           
-            <View style={styles.inputGroup}>
-                <Picker selectedValue={state.destinatario} 
-                    onValueChange={(itemValue) => handleChangeText("destinatario", itemValue)}>
-                    {
-                        destinatarios.map((v) => <Picker.Item key={v.key} label={v.label} value={v.value} />)
-                    }
-
-                </Picker>
-            </View>
-
-            <View style={styles.inputGroup}>
-                <TextInput
-                    placeholder='Cuerpo:'
-                    onChangeText={(value) => handleChangeText("cuerpo", value)}
-                />
-            </View>
-            <Image
-                source = {{uri:imageLocal}}
-                style={{width: 200, height: 200}}
-
+        
+        <ScrollView  style={styles.container}>
+            <Header style={{color:'#b10909'}}
+                centerComponent={{ text: 'PUBLICACIONES', style: { color: '#fff' } }}
+                containerStyle={{
+                    backgroundColor: '#b10909',
+                    justifyContent: 'space-around',}}
             />
-            <Button
-                title='Añadir Archivo'
-                theme={{ colors: { primary: '#8E0101' } }}
-                icon={<Icon name='file-upload' color='#ffffff' />}
-                buttonStyle={{ borderRadius: 10, marginLeft: 0, marginRight: 0, marginBottom: 10 }}
-                onPress={mostrarfotoGalaria} />
+            
+                <View style={styles.Pheader}></View>
 
-            {uploading && (
-                <View>
-                    <ProgressBar progress={progress} />
-                    <Subheading>{parseInt(progress*100)+' %'}</Subheading>
-                </View>
-            )}
-            {downloadURL && (
-              <Button title='Publicar' onPress={() => saveNewPublication()}  ></Button>
-            )}
+                <View style={styles.Pbody}>
+                    <View style={{marginTop:10}}>
+                        <Text style={styles.TextGroup}>Imagen:</Text>
+                        <View style={{flexDirection:'row', marginTop:10,marginLeft:15,alignItems:'center'}}>
+                            <TextInput
+                                style={{width:'60%', backgroundColor:'white',borderRadius:10} }
+                                placeholder='Subir Imagen:'
+                            />
+                            
+                            <Button title="Subir..." 
+                            buttonStyle={{height:30,width:70, marginLeft:10,borderRadius:12}}
+                            onPress={() => refRBSheet.current.open()} /> 
+                        </View>
+                       {downloadURL &&(
+                            <Image 
+                                source = {{uri:imageLocal}}
+                                style={{width: 255, height: 200, alignSelf:'center',marginTop:15, marginRight:5}}
+                            /> 
+                            
+                       )}
+                        
+                    </View>
 
+                
 
-            <Button title="OPEN BOTTOM SHEET" onPress={() => refRBSheet.current.open()} />
-               
-
-            <RBSheet
-                ref={refRBSheet}
-                closeOnDragDown={true}
-                closeOnPressMask={false}
-                height={180}
-                customStyles={{
-                    
-                    wrapper: {
-                        backgroundColor: 'rgba(0,0,0,0.5)',
-                    },
-                    draggableIcon: {
-                        backgroundColor: '#ffc604'
-                    }
-                }}
-            >
-                <View style={{flexDirection:'column', justifyContent:'center', alignContent:'center', marginTop:'2%'}}>
-                    <TouchableOpacity 
-                        onPress={tomarFotoCamara}
-                        style={{
-                            flexDirection:'row', 
-                            alignItems:'center', 
-                            marginBottom:'2%'
-                        }}>
-                        <IconButton
-                            icon='camera'
-                            size={30}
-                            color={'grey'}
+                    <View >
+                        <Text style={styles.TextGroup}>Titulo:</Text>
+                        <TextInput
+                            style={styles.inputGroup}
+                            placeholder='Titulo:'
+                            onChangeText={(value) => handleChangeText("titulo", value)}
                         />
-                        <Subheading style={{fontFamily:'Montserrat-Medium'}}>Tomar foto</Subheading>
-                    </TouchableOpacity>
-                    <TouchableOpacity 
-                        onPress={mostrarfotoGalaria}
-                        style={{
-                            flexDirection:'row',  
-                            alignItems:'center'
+                    </View>
+                    
+                    <View style={styles.inputGroup}>
+                        <Picker selectedValue={state.destinatario} 
+                            onValueChange={(itemValue) => handleChangeText("destinatario", itemValue)}>
+                            {
+                                destinatarios.map((v) => <Picker.Item key={v.key} label={v.label} value={v.value} />)
+                            }
+
+                        </Picker>
+                    </View>
+                    <Text style={styles.TextGroup}>Cuerpo:</Text>
+                    <View>
+                    
+                        <TextInput
+                            style={styles.inputGroup}
+                            multiline
+                            numberOfLines={8}
+                            placeholder='Cuerpo:'
+                            onChangeText={(value) => handleChangeText("cuerpo", value)}
+                        />
+                    </View>
+                    <View style={{marginTop:15}}></View>
+                    
+
+
+                    {downloadURL && (
+                    <Button title='Publicar'
+                    buttonStyle={{marginTop:15, width:'70%', alignSelf:'center',borderRadius:15}}
+                    onPress={() => saveNewPublication()}  ></Button> )}
+
+                    
+                    
+                    
+
+                
+
+                    {uploading && (
+                        <View>
+                            <ProgressBar progress={progress} />
+                            <Subheading>{parseInt(progress*100)+' %'}</Subheading>
+                        </View>
+                    )}
+                    
+                    
+
+                    <RBSheet
+                        ref={refRBSheet}
+                        closeOnDragDown={true}
+                        closeOnPressMask={false}
+                        height={180}
+                        customStyles={{
+                            
+                            wrapper: {
+                                backgroundColor: 'rgba(0,0,0,0.5)',
+                            },
+                            draggableIcon: {
+                                backgroundColor: '#ffc604'
+                            }
                         }}
                     >
-                        <IconButton
-                            icon='image-multiple'
-                            size={30}
-                            color={'grey'}
+                        <View style={{flexDirection:'column', justifyContent:'center', alignContent:'center', marginTop:'2%'}}>
+                            <TouchableOpacity 
+                                onPress={tomarFotoCamara}
+                                style={{
+                                    flexDirection:'row', 
+                                    alignItems:'center', 
+                                    marginBottom:'2%'
+                                }}>
+                                <IconButton
+                                    icon='camera'
+                                    size={30}
+                                    color={'grey'}
+                                />
+                                <Subheading style={{fontFamily:'Montserrat-Medium'}}>Tomar foto</Subheading>
+                            </TouchableOpacity>
+                            <TouchableOpacity 
+                                onPress={mostrarfotoGalaria}
+                                style={{
+                                    flexDirection:'row',  
+                                    alignItems:'center'
+                                }}
+                            >
+                                <IconButton
+                                    icon='image-multiple'
+                                    size={30}
+                                    color={'grey'}
 
-                        />
-                        <Subheading style={{fontFamily:'Montserrat-Medium'}}>Seleccionar de galería</Subheading>
-                    </TouchableOpacity>
+                                />
+                                <Subheading style={{fontFamily:'Montserrat-Medium'}}>Seleccionar de galería</Subheading>
+                            </TouchableOpacity>
+                            
+                        </View>
+                    </RBSheet>
                     
-                </View>
-            </RBSheet>
-            
-    
-        </View >
+                </View>          
+                <Subheading style={{height:50,backgroundColor:'#b10909',flex: 1, justifyContent: 'space-between'}}></Subheading>
+                
+        </ScrollView  >
+        
 
     )
 }
+/*
+{downloadURL && (
+                <Button title='Publicar' onPress={() => saveNewPublication()}  ></Button>
+                )}
+
+                
+                <Button title="OPEN BOTTOM SHEET" onPress={() => refRBSheet.current.open()} />
+                */
