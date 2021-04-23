bb42fc2acf013e96d9d53ac44e802bc4a1c470d1
unknown <cristhian.andrade@uao.edu.co>
2021-04-23 12:41:37 -0500

    Se añadieron estilos al campo de Listar Usuarios, un salto de BackGround color blanco - gris asi sucesivamente, se agrego un header y se manipulo los margenes para que ocupara todo el espacio de la pantalla

--- src/Layout/Auth/ListUsers.js
+++ src/Layout/Auth/ListUsers.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { ActivityIndicator, FlatList, View, StyleSheet } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
-import { ListItem, Icon } from 'react-native-elements';
-import {Avatar} from 'react-native-paper'
+import { ListItem, Icon, Header } from 'react-native-elements';
+import {Avatar,Subheading} from 'react-native-paper'
+
 
 export default function ListUsers({navigation}){
 //const ListUsers = props => {
@@ -14,7 +15,8 @@ export default function ListUsers({navigation}){
     correo: "",
     id_rol :"",
     grupo :"",
-    url :""
+    url :"",
+    contador:0,
   });
    
   
@@ -43,7 +45,7 @@ export default function ListUsers({navigation}){
       const renderAvatar = (image, iniciales) =>{
         if(image===null || image===undefined){
           const renderAvatarText = () => (
-            <Avatar.Text style={{alignSelf: 'center', backgroundColor:'#b31d1d'}}
+            <Avatar.Text style={{alignSelf: 'center', backgroundColor:'#b31d1d', marginLeft:10}}
             size={60} 
             label={iniciales}
             />
@@ -51,7 +53,7 @@ export default function ListUsers({navigation}){
           return renderAvatarText();
         }else{
           const renderAvatarImage = () => (
-            <Avatar.Image style={{alignSelf: 'center'}}
+            <Avatar.Image style={{alignSelf: 'center', marginLeft:10}}
             size={60} 
             source={{
             uri: image || 'https://reactnativeelements.com//img/avatar/avatar--edit.jpg'
@@ -62,29 +64,192 @@ export default function ListUsers({navigation}){
         }
       }
 
+      
+  getTempRow = (t) => {
+    if(t==1){
+      
+      users.contador=2;
+      return 1
+    }
+    else{
+      users.contador=1;
+      return 2
+    }
+  }
+
    return (
-    <FlatList
-      data={users}
-      renderItem={({ item }) => (
-          <View style={styles.profileHeader}>
-            <ListItem key={item.id} >
-                  {renderAvatar(item.url, (item.nombres.charAt(0) + item.apellidos.charAt(0)))}
-                <ListItem.Content>
-                    <ListItem.Title>{item.nombres+ ' ' + item.apellidos}</ListItem.Title>
-                    <ListItem.Subtitle>{item.id_grupo}</ListItem.Subtitle>
-                </ListItem.Content>
-               <Icon
-                name = 'pencil-square-o'
-                type='font-awesome'
-                onPress={()=>navigation.navigate('EditProfile',{state:item, page:'listUsers'})}
-               />
-          </ListItem>
-      </View>
-      )}
-    />
+    <View>
+      <Header style={{color:'#b10909'}}
+          centerComponent={{ text: 'USUARIOS', style: { color: '#fff' } }}
+          containerStyle={{
+          backgroundColor: '#b10909',
+          justifyContent: 'space-around',}}>
+      </Header>
+
+      <FlatList
+      
+        data={users}
+        renderItem={({ item }) => (
+            <View style={styles.profileHeader}>
+              <ListItem key={item.id} >
+                <View
+                style={[
+                  (getTempRow(users.contador)== 1) ? styles.colorback:styles.colorbackoriginal]}>
+
+                    {renderAvatar(item.url, (item.nombres.charAt(0) + item.apellidos.charAt(0)))}
+                    <ListItem.Content style={{marginLeft:15}}>
+                        <ListItem.Title>{item.nombres+ ' ' + item.apellidos}</ListItem.Title>
+                        <ListItem.Subtitle>{item.id_grupo}</ListItem.Subtitle>
+                    </ListItem.Content>
+                  <Icon
+                    containerStyle={{marginRight:20}}
+                    name = 'pencil-square-o'
+                    type='font-awesome'
+                    onPress={()=>navigation.navigate('EditProfile',{state:item, page:'listUsers'})}
+                  />
+                </View>
+            </ListItem>
+            
+        </View>
+        )}
+        >
+        
+        </FlatList>
+      
+      
+    </View>
   );
   
 }
 const styles = StyleSheet.create({
-    
+    colorback:{
+      height:100,
+      width:'100%',
+      backgroundColor:'#dfdfdf',
+      flexDirection:'row',
+      justifyContent:'center',
+      alignItems:'center',
+      
+      
+      
+      
+    },
+    colorbackoriginal:{
+      height:68,
+      width:'100%',
+      flexDirection:'row',
+      justifyContent:'center',
+      alignItems:'center',
+      
+    },
+    profileHeader:{
+      width:'106%',
+      margin:-8,
+    }
   });
+/*
+  style={[
+    (getTempRow(users.contador)== 1) ? styles.colorback:styles.colorbackoriginal]}
+*/
+
+
+
+/*
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, FlatList, View, StyleSheet,ScrollView } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import { ListItem , Avatar, Icon, Header } from 'react-native-elements';
+//import {Avatar} from 'react-native-paper'
+
+
+export default function ListUsers(){
+//const ListUsers = props => {
+  
+  const [users, setUsers] = useState({
+    contador:0,
+  }); 
+  
+   // const [users, setUsers] = useState(false)
+  
+  
+  useEffect(() => {
+      const subscriber = firestore()
+        .collection('Usuario')
+        .onSnapshot(querySnapshot => {
+          const users = [];
+    
+          querySnapshot.forEach(documentSnapshot => {
+            users.push({
+              ...documentSnapshot.data(),
+              key: documentSnapshot.id,
+            });
+          });
+    
+          setUsers(users);
+          //setLoading(false);
+        });
+      
+        // Unsubscribe from events when no longer in use
+        return () => subscriber();
+      }, []);
+
+  
+
+  getTempRow = (t) => {
+    if(t==1){
+      
+      users.contador=2;
+      return 1
+    }
+    else{
+      users.contador=1;
+      return 2
+    }
+  }
+
+    
+   return (
+     <View>
+       <Header style={{color:'#b10909'}}
+                centerComponent={{ text: 'USUARIOS', style: { color: '#fff' } }}
+                containerStyle={{
+                    backgroundColor: '#b10909',
+                    justifyContent: 'space-around',}}
+            />
+      <FlatList
+      
+        data={users}
+        renderItem={({ item }) => (
+            <View style={styles.profileHeader}>
+              <ListItem key={item.id}>
+                <View
+                  style={[
+                    (getTempRow(users.contador)== 1) ? styles.colorback:styles.colorbackoriginal]}
+                  >
+                    <Avatar rounded
+                            containerStyle={{marginLeft:10}}
+                            source={{
+                            uri: item.url,
+                            }}
+                            size={80}
+                        />
+                    <ListItem.Content style={{margin:10}}>
+                        <ListItem.Title>{item.nombres+ ' ' + item.apellidos}</ListItem.Title>
+                        <ListItem.Subtitle>{item.id_grupo}</ListItem.Subtitle>
+                    </ListItem.Content>
+                  
+                  <Icon
+                  
+                  icon = 'page-last'
+                  
+                  />
+                </View>
+              </ListItem>
+        </View>
+        )}
+      />
+    </View>
+  );
+  
+};
+*/
