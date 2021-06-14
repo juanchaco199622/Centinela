185cb6f8ba40ae5ac2d021bbf7602ad6b0b06b4f
juanchaco199622 <juan199622@hotmail.com>
2021-06-13 22:45:18 -0500

    CAMBIOS

--- src/Components/Notifications/DeleteNotificationAsk.js
+++ src/Components/Notifications/DeleteNotificationAsk.js
@@ -23,14 +23,7 @@ const DeleteNotificationAsk = (props) => {
     const [asks,setAsks] = useState([]);
     const [loading,setLoading] = useState(true);
 
-    state = {
-        modalVisible: false
-      };
-    
-      setModalVisible = (visible) => {
-        this.setState({ modalVisible: visible });
-      }
-      const { modalVisible } = state;
+   
 
 
     const leftSwipe = (progress, dragX) => {
@@ -58,35 +51,6 @@ const DeleteNotificationAsk = (props) => {
         
         <Swipeable renderLeftActions={leftSwipe}>
 
-            <View style={styles.centeredView}>
-                    <Modal
-                    animationType="slide"
-                    transparent={true}
-                    visible={modalVisible}
-                    onRequestClose={() => {
-                        Alert.alert("Modal has been closed.");
-                        this.setModalVisible(!modalVisible);
-                    }}
-                    >
-                    <View style={styles.centeredView}>
-                        <View style={styles.modalView}>
-                        <Text style={styles.modalText}>Hello World!</Text>
-                        <Pressable
-                            style={[styles.button, styles.buttonClose]}
-                            onPress={() => this.setModalVisible(!modalVisible)}
-                        >
-                            <Text style={styles.textStyle}>Hide Modal</Text>
-                        </Pressable>
-                        </View>
-                    </View>
-                    </Modal>
-                    <Pressable
-                    style={[styles.button, styles.buttonOpen]}
-                    onPress={() => this.setModalVisible(true)}
-                    >
-                    <Text style={styles.textStyle}>Show Modal</Text>
-                    </Pressable>
-                </View>
             <View style={styles.container}>
                 
                 <View style={{flexDirection:'row', justifyContent:'space-between', width:'80%'}}>
